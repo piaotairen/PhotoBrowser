b24d9a599311cb24954429f233eb5654e4505817
Cobb <huzhiyuan@onething.net>
2018-12-10 14:06:33 +0800

    Initial Commit

---
+++ PhotoBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoBrowser
+//
+//  Created by Cobb on 2018/12/10.
+//  Copyright © 2018 Cobb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
